39ee31aefd0fc5bcee4e09a12fb2d976736eea74
Bart Webber and Trevor Gill <bart@>
2017-07-17 16:19:35 -0700

    add remove button to remove list item

---
+++ js/scripts.js
@@ -0,0 +1,66 @@
+var array = [];
+
+//Business Logic
+
+function ListItem(item, count, price) {
+  this.item = item;
+  this.count = count;
+  this.price = price;
+}
+
+
+
+// user interface Logic
+$(document).ready(function() {
+  $("form#new-list").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedListItem = $("input#new-list-item").val();
+    var inputtedListCount = $("input#new-list-count").val();
+    var inputtedListPrice = $("input#new-list-price").val();
+
+
+    var newListItem = new ListItem(inputtedListItem, inputtedListCount, inputtedListPrice);
+
+    $(".listItem").append("<li><input type='checkbox' class='listItem' name='grocery-thing' value='" + newListItem.item + "'>" + " " + "<span class='click-me'>" + newListItem.item + "</span></li>");
+
+    $("input#new-list-item").val("");
+
+
+    $(".click-me").last().click(function() {
+    $("#show-list").show();
+    $("#show-list h2").text(newListItem.item);
+    $(".count").text(newListItem.count);
+    $(".price").text(newListItem.price);
+    });
+  });
+
+
+
+
+
+
+  $("form#list-edit").submit(function(event) {
+    event.preventDefault();
+
+    $("input:checkbox[name=grocery-thing]:checked").each(function(){
+      var listEditSelection = $(this).val();
+
+
+
+    if(listEditSelection=$(this).val()) {
+      $(this).parent().remove();
+      // var array2 = array.push($(this).val());
+
+
+    //   // alert($(this).val());
+    //   var listEditStrike = listEditSelection.replace(/listEditSelection/i, listEditSelection.strike());
+      // alert(listEditStrike);
+
+
+    }
+
+
+    });
+  });
+});
